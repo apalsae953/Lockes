acf02e13d86c7102a788eb93b353307b47dc24a9
apalsae953 <antoniopalsae@gmail.com>
2026-06-22 17:51:33 +0200

    fix(cors): add capacitor mobile origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,7 +22,11 @@ return [
     'allowed_origins' => [
         'https://lockes.vercel.app',
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'http://localhost',
+        'capacitor://localhost',
+        'ionic://localhost',
+        'http://10.0.2.2:5173',
     ],
 
     'allowed_origins_patterns' => [],
