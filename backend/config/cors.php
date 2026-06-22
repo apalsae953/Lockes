f3c72c58dcd94668b8c45dcefea721a4f74465f4
apalsae953 <antoniopalsae@gmail.com>
2026-06-22 18:17:51 +0200

    fix(cors): allow wildcard origins for capacitor

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,13 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://lockes.vercel.app',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost',
-        'capacitor://localhost',
-        'ionic://localhost',
-        'http://10.0.2.2:5173',
+        '*',
     ],
 
     'allowed_origins_patterns' => [],
